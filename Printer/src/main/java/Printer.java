d8337857c3380e10d1bf4564ccd758dcd87d4a9c
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-08 16:45:27 +0100

    Printer getName() created and tested

--- Printer/src/main/java/Printer.java
+++ Printer/src/main/java/Printer.java
@@ -1,2 +1,12 @@
 public class Printer {
+
+    private String name;
+
+   public Printer(String name){
+       this.name = name;
+   }
+
+    public String getName() {
+        return this.name;
+    }
 }
--- Printer/src/test/java/PrinterTest.java
+++ Printer/src/test/java/PrinterTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer("printer1");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("printer1", printer.getName());
+    }
+
+
+
 }
