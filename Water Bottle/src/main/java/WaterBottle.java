c6367acc1e412a9a19be5d8356d80f5732d720f8
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-08 16:24:11 +0100

    WaterBottle has name created and tested

--- Water Bottle/src/main/java/WaterBottle.java
+++ Water Bottle/src/main/java/WaterBottle.java
@@ -1,2 +1,11 @@
 public class WaterBottle {
+    private String name;
+
+    public WaterBottle(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 }
--- Water Bottle/src/test/java/WaterBottleTest.java
+++ Water Bottle/src/test/java/WaterBottleTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle("bottle1");
+    }
+
+    @Test
+    public void bottleHasName(){
+        assertEquals("bottle1", waterBottle.getName());
+    }
 }
